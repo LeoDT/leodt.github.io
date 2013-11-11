947010f9b7f3c55879f58ea2add727fb023644e0
LeoDT <leojoy710@gmail.com>
2013-11-11 16:35:30 +0800

    basic tab change

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 /**
  * Created by LeoDT on 13-11-11.
  */
+(function(){
+    var panes = $('.pane'),
+        tabs = $('.tab'),
+        change_tab = function(){
+            var hash = location.hash;
+            panes.removeClass('show');
+            panes.filter(hash).addClass('show');
+            tabs.removeClass('active');
+            tabs.filter('a[href="' + hash + '"]').addClass('active');
+        };
+
+    $(window).on('hashchange', function(e){
+        change_tab();
+    });
+    change_tab();
+})();
