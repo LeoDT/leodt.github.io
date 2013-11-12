196cd1f6a8cab99cad2f6dcc329a418074fca8c5
LeoDT <leojoy710@gmail.com>
2013-11-12 11:20:38 +0800

    switch transition finished

--- js/main.js
+++ js/main.js
@@ -4,16 +4,96 @@
 (function(){
     var panes = $('.pane'),
         tabs = $('.tab'),
+        skills = $('.skill'),
+        links = $('.link'),
+        last_hash,
+        slide_in = function(hash){
+            panes.filter(hash).addClass('show');
+            setTimeout(function(){
+                panes.filter(hash).addClass('in');
+            }, 0);
+        },
+        trans_out = {
+            'home': function(next, hash){
+                panes.removeClass('in');
+                setTimeout(function(){
+                    panes.removeClass('show');
+                    next(hash);
+                }, 300);
+            },
+            'about': function(next, hash){
+                panes.removeClass('in');
+                setTimeout(function(){
+                    panes.removeClass('show');
+                    next(hash);
+                }, 300);
+            },
+            'skills': function(next, hash){
+                skills.each(function(i, el){
+                    el = $(el);
+                    setTimeout(function(){
+                        el.removeClass('in');
+
+                        if(i + 1 == skills.length){
+                            next(hash);
+                        }
+                    }, i*120);
+                });
+            },
+            'links': function(next, hash){
+                links.each(function(i, el){
+                    el = $(el);
+                    setTimeout(function(){
+                        el.removeClass('in');
+
+                        if(i + 1 == links.length){
+                            next(hash);
+                        }
+                    }, i*120);
+                });
+            }
+        },
+        trans_in = {
+            'home': slide_in,
+            'about': slide_in,
+            'skills': function(hash){
+                panes.filter(hash).addClass('show');
+
+                skills.each(function(i, el){
+                    el = $(el);
+                    setTimeout(function(){
+                        el.addClass('in');
+                    }, i*120);
+                });
+            },
+            'links': function(hash){
+                panes.filter(hash).addClass('show');
+
+                links.each(function(i, el){
+                    el = $(el);
+                    setTimeout(function(){
+                        el.addClass('in');
+                    }, i*120);
+                });
+            }
+        },
         change_tab = function(){
             var hash = location.hash;
             if(!hash){
                 hash = '#home';
             }
 
-            panes.removeClass('show');
-            panes.filter(hash).addClass('show');
+            if(last_hash){
+                trans_out[last_hash.slice(1)](trans_in[hash.slice(1)], hash);
+            }
+            else{
+                trans_in[hash.slice(1)](hash);
+            }
+
             tabs.removeClass('active');
             tabs.filter('a[href="' + hash + '"]').addClass('active');
+
+            last_hash = hash;
         };
 
     $(window).on('hashchange', function(e){
